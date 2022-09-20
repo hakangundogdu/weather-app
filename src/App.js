c2402a436f6185cf9b14731a99d31959888bf079
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-09-20 14:57:39 +0300

    Error and spinner

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Slider from './components/Slider';
 import Spinner from './components/Spinner';
 
 function App() {
-  const { getAllData, allData } = useWeather();
+  const { getAllData, allData, error } = useWeather();
 
   useEffect(() => {
     getAllData();
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <Layout>
-      {allData.length === 27 ? (
+      {allData.length === 27 && (
         <>
           <div className="mt-6 grid md:h-96 grid-cols-1 gap-y-10 gap-x-6 md:grid-cols-3  ">
             <div className=" md:col-span-2 h-96 w-full rounded-xl border overflow-hidden ">
@@ -25,8 +25,13 @@ function App() {
           </div>
           <Slider />
         </>
-      ) : (
-        <Spinner />
+      )}
+      {error || allData.length === 27 ? null : <Spinner />}
+
+      {error && (
+        <p className="text-center mt-36 text-xl text-slate-900">
+          {'Could not fetch weather data!'}
+        </p>
       )}
     </Layout>
   );
--- src/store/weather-context.js
+++ src/store/weather-context.js
@@ -8,13 +8,13 @@ const WeatherContext = createContext();
 
 export const WeatherContextProvider = (props) => {
   const [allData, setAllData] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState(false);
   const [day, setDay] = useState(0);
   const [selectedCity, setSelectedCity] = useState('Sofia');
 
   const getAllData = async () => {
-    try {
-      const getCityData = async (city) => {
+    const getCityData = async (city) => {
+      try {
         const response = await axios.get(
           `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&exclude=hourly,minutely,alerts&appid=${apiKey}&units=metric`
         );
@@ -28,71 +28,15 @@ export const WeatherContextProvider = (props) => {
             daily: response.data.daily,
           },
         ]);
-      };
+      } catch (error) {
+        setError(true);
+      }
+    };
 
-      cityDetail.forEach((city) => {
-        getCityData(city);
-      });
-    } catch (error) {
-      setError('Could not fetch weather.');
-    }
+    cityDetail.forEach((city) => {
+      getCityData(city);
+    });
   };
-
-  // useEffect(() => {
-  //   const filteredData = allData.filter((city) => city.name === selectedCity);
-
-  //   setCityData(filteredData);
-  // }, [selectedCity, allData]);
-
-  // const getAllCurrent = async () => {
-  //   try {
-  //     const fetchCityWeather = async (city) => {
-  //       const response = await axios.get(
-  //         `https://api.openweathermap.org/data/2.5/weather?q=${city},bg&appid=${apiKey}&units=metric`
-  //       );
-
-  //       setCityArray((prev) => [
-  //         ...prev,
-  //         {
-  //           id: response.data.id,
-  //           name: response.data.name,
-  //           lat: response.data.coord.lat,
-  //           lon: response.data.coord.lon,
-  //         },
-  //       ]);
-  //       setCurrentData((prev) => [...prev, response.data]);
-  //     };
-
-  //     cities.forEach((city) => {
-  //       fetchCityWeather(city);
-  //     });
-  //   } catch (error) {
-  //     setError('Could not fetch weather.');
-  //   }
-  // };
-
-  // const getCityData = async (city) => {
-  //   try {
-  //     const response = await axios.get(
-  //       `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&exclude=hourly,minutely,alerts&appid=${apiKey}&units=metric`
-  //     );
-  //     setCityData(response.data);
-  //   } catch (error) {
-  //     setError('Could not fetch weather.');
-  //   }
-  // };
-
-  // const getForecast = async ({ city }) => {
-  //   try {
-  //     const resForecast = await axios.get(
-  //       `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&exclude=hourly,minutely,alerts&appid=${apiKey}&units=metric`
-  //     );
-  //     setForecastData(resForecast.data);
-  //   } catch (error) {
-  //     setError('Could not fetch weather.');
-  //   }
-  // };
-
   const contextValue = {
     error,
     getAllData,
