5b4136e4ec1b4929d7394d7bef2428156861a708
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-09-20 13:25:51 +0300

    formatting

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,12 +3,22 @@ import { dateFormat } from '../lib/date';
 
 const Box = () => {
   const { allData, selectedCity, day } = useWeather();
-
   const filteredData = allData.filter((city) => city.name === selectedCity);
+
+  const weather = {
+    date: dateFormat(filteredData[0].daily[day].dt).date,
+    day: dateFormat(filteredData[0].daily[day].dt).day,
+    icon: `http://openweathermap.org/img/wn/${filteredData[0].daily[day].weather[0].icon}@2x.png`,
+    name: filteredData[0].name,
+    degree: filteredData[0].daily[day].feels_like.day.toFixed(0),
+    desc: filteredData[0].daily[day].weather[0].description,
+  };
+
+  console.log(weather.desc[0] + weather.desc[1] + weather.desc[2]);
 
   return (
     <>
-      <div className="col-span-1 p-6 flex flex-col rounded-xl border-2 w-full h-full border-slate-300 overflow-hidden ">
+      <div className="col-span-1 p-10 flex flex-col  gap-y-2 rounded-xl border w-full h-full bg-sky-100 shadow-lg overflow-hidden ">
         {allData.length === 27 ? (
           <>
             <p className="text-sky-500 font-semibold">
@@ -23,20 +33,19 @@ const Box = () => {
               src={`http://openweathermap.org/img/wn/${filteredData[0].daily[day].weather[0].icon}@2x.png`}
               alt=""
             />
-            <p className="text-slate-900">
-              {dateFormat(filteredData[0].daily[day].dt).day}
+            <p className="text-slate-900 text-2xl font-bold">
+              {weather.degree} ºC
             </p>
 
-            <p className="text-slate-900">
-              {filteredData[0].daily[day].weather[0].description}
+            <p className="text-slate-600">
+              {weather.desc[0].toUpperCase()}
+              {weather.desc.slice(1)}
             </p>
-            <p className="text-slate-900">
-              Feels Like: {filteredData[0].daily[day].feels_like.day}
-            </p>
-            <p className="text-slate-900">
+
+            <p className="text-slate-600">
               Humidity: {filteredData[0].daily[day].humidity}
             </p>
-            <p className="text-slate-900">
+            <p className="text-slate-600">
               Pressure: {filteredData[0].daily[day].pressure}
             </p>
           </>
